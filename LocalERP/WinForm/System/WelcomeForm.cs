dfcc58578046a9eb05279e99302afcd1532639aa
MrBingo2008 <267298093@qq.com>
2019-11-03 21:48:00 +0800

    welcome form的快捷链接开始修改为后台生成

--- LocalERP/WinForm/System/WelcomeForm.cs
+++ LocalERP/WinForm/System/WelcomeForm.cs
@@ -19,12 +19,30 @@ namespace LocalERP.WinForm
         {
             this.mainForm = form;
             InitializeComponent();
+
+            addRow();
         }
 
         private void linkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             LinkLabel linkLabel = sender as LinkLabel;
             mainForm.setForm(linkLabel.Text);
+        }
+
+        private void addRow() {
+            Label label = new System.Windows.Forms.Label();
+            
+            label.Font = new System.Drawing.Font("宋体", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            label.Image = global::LocalERP.Properties.Resources.Yes;
+            label.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            label.Location = new System.Drawing.Point(74, 55);
+            label.Name = "label1";
+            label.Size = new System.Drawing.Size(100, 18);
+            label.TabIndex = 58;
+            label.Text = "采购管理";
+            label.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+
+            this.Controls.Add(label);
         }
     }
 }
